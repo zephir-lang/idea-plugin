90e29988e033108c6b2e2e01fbbba0b8456f50b0
Nikita Gusakov <dev@nkt.me>
2014-09-22 22:36:28 +0400

    Improve commenter

--- src/com/zephir/lang/ZephirCommenter.java
+++ src/com/zephir/lang/ZephirCommenter.java
@@ -1,12 +1,15 @@
 package com.zephir.lang;
 
-import com.intellij.lang.Commenter;
+import com.intellij.lang.CodeDocumentationAwareCommenter;
+import com.intellij.psi.PsiComment;
+import com.intellij.psi.tree.IElementType;
+import com.zephir.psi.ZephirTypes;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Nikita Gusakov
  */
-public class ZephirCommenter implements Commenter {
+public class ZephirCommenter implements CodeDocumentationAwareCommenter {
     @Nullable
     @Override
     public String getLineCommentPrefix() {
@@ -28,12 +31,53 @@ public class ZephirCommenter implements Commenter {
     @Nullable
     @Override
     public String getCommentedBlockCommentPrefix() {
-        return "/*";
+        return null;
     }
 
     @Nullable
     @Override
     public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public IElementType getLineCommentTokenType() {
+        return ZephirTypes.COMMENT;
+    }
+
+    @Nullable
+    @Override
+    public IElementType getBlockCommentTokenType() {
+        return ZephirTypes.COMMENT_BLOCK;
+    }
+
+    @Nullable
+    @Override
+    public IElementType getDocumentationCommentTokenType() {
+        return ZephirTypes.COMMENT_BLOCK;
+    }
+
+    @Nullable
+    @Override
+    public String getDocumentationCommentPrefix() {
+        return "/**";
+    }
+
+    @Nullable
+    @Override
+    public String getDocumentationCommentLinePrefix() {
+        return "*";
+    }
+
+    @Nullable
+    @Override
+    public String getDocumentationCommentSuffix() {
         return "*/";
+    }
+
+    @Override
+    public boolean isDocumentationComment(PsiComment element) {
+        return element.getTokenType() == ZephirTypes.CODE_BLOCK;
     }
 }
