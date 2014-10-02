2d4e6c7b07199164d6816959d20f8598a831eee5
Nikita Gusakov <dev@nkt.me>
2014-10-02 09:10:23 +0400

    Initial support for "new file" actions

--- src/com/zephir/actions/ZephirNewClassAction.java
+++ src/com/zephir/actions/ZephirNewClassAction.java
@@ -1,9 +1,13 @@
 package com.zephir.actions;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.actions.CreateFileFromTemplateAction;
 import com.intellij.ide.actions.CreateFileFromTemplateDialog;
+import com.intellij.ide.fileTemplates.FileTemplate;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
 import com.zephir.ZephirIcons;
 
 /**
@@ -12,18 +16,24 @@ import com.zephir.ZephirIcons;
 public class ZephirNewClassAction extends CreateFileFromTemplateAction {
 
     public ZephirNewClassAction() {
-        super("Zephir Class", "Create new Zephir class", ZephirIcons.PHALCON);
+        super("Zephir Class", "Create new Zephir file", ZephirIcons.PHALCON);
     }
 
     @Override
-    protected void buildDialog(Project project, PsiDirectory psiDirectory, CreateFileFromTemplateDialog.Builder builder) {
-        builder.setTitle("New Zephir class");
-        builder.addKind("Class", null, "class");
-        builder.addKind("Interface", null, "interface");
+    protected void buildDialog(Project project, PsiDirectory directory, CreateFileFromTemplateDialog.Builder builder) {
+        builder.setTitle("New Zephir file");
+        builder.addKind("Class", AllIcons.Nodes.Class, "Zephir Class");
+        builder.addKind("Interface", AllIcons.Nodes.Interface, "Zephir Interface");
     }
 
     @Override
-    protected String getActionName(PsiDirectory psiDirectory, String s, String s2) {
+    protected String getActionName(PsiDirectory directory, String name, String template) {
         return "Creating new Zephir class";
+    }
+
+    @Override
+    protected PsiFile createFile(String name, String templateName, PsiDirectory dir) {
+        final FileTemplate template = FileTemplateManager.getInstance().getInternalTemplate(templateName);
+        return createFileFromTemplate(name, template, dir);
     }
 }
