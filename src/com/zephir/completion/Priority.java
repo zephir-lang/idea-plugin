9d77d9fa0ee71e11681f6f9a24d1145170f5d9a3
Alex Baranezky <asterai.com@gmail.com>
2017-05-23 23:19:32 +0300

    Added class members to completion list

--- src/com/zephir/completion/Priority.java
+++ src/com/zephir/completion/Priority.java
@@ -6,5 +6,7 @@ package com.zephir.completion;
 public class Priority {
 
     public static double METHOD_SCOPE_PRIORITY = 2.0;
-
+    public static double CLASS_METHOD_PRIORITY = 1.7;
+    public static double CLASS_CONSTS_PRIORITY = 1.6;
+    public static double CLASS_PROPERTY_PRIORITY = 1.5;
 }
