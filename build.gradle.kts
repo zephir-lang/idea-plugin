bbf1a8be64a952337bdfc697ec6979626de86b73
Serghei Iakovlev <egrep@protonmail.ch>
2020-07-08 23:45:04 +0300

    Cleaned up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,25 +9,27 @@
 import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 import org.jetbrains.grammarkit.tasks.GenerateLexer
 import org.jetbrains.grammarkit.tasks.GenerateParser
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.gradle.api.internal.HasConvention
 
 group = "com.zephir"
-version = project.properties["version"].toString()
-description = "Zephir plugin for the IntelliJ Platform"
+version = prop("version")
+description = prop("pluginDescription")
 
 repositories {
     jcenter()
-}
-
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    testImplementation("junit:junit:4.12")
 }
 
 plugins {
     idea
     id("org.jetbrains.intellij") version "0.4.21"
     id("org.jetbrains.grammarkit") version "2020.1"
+    id("net.saliman.properties") version "1.5.1"
     kotlin("jvm") version "1.3.72"
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 idea {
@@ -38,60 +40,20 @@ idea {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    pluginName = project.properties["pluginName"].toString()
-    version = project.properties["ideVersion"].toString()
+    pluginName = prop("pluginName")
+    version = prop("ideVersion")
     updateSinceUntilBuild = false
 }
 
 sourceSets {
     main {
         java.srcDirs("gen")
+        kotlin.srcDirs("src/main/kotlin")
+        resources.srcDirs("src/main/resources")
     }
-}
-
-fun readChangeNotes(pathname: String, version: String): String? {
-    val changeLog = file(pathname)
-    if (!changeLog.exists()) {
-        return null
-    }
-
-    val lines = changeLog.readLines()
-    val notes: MutableList<MutableList<String>> = mutableListOf()
-    var note: MutableList<String>? = null
-    var found = false
-
-    for (line in lines) {
-        if (!line.startsWith("## [$version") && !found) {
-            continue
-        }
-
-        if (line.startsWith("## [") && found) {
-            break
-        }
-
-        if (line.startsWith("## [$version")) {
-            note = mutableListOf()
-            notes.add(note)
-            found = true
-
-            continue
-        }
-
-        if (line.startsWith("- ") && found) {
-            note?.add(
-                line.trimStart('-', ' ').replace("`(.*?)`".toRegex()) {
-                    "<code>${it.groupValues[1]}</code>"
-                }
-            )
-        }
-    }
-
-    if (notes.size == 0) {
-        return null
-    }
-
-    return notes.joinToString("</li><li>", prefix = "<ul><li>", postfix = "</li></ul>") {
-        it.joinToString("</li><li>")
+    test {
+        kotlin.srcDirs("src/test/kotlin")
+        resources.srcDirs("src/test/resources")
     }
 }
 
@@ -104,7 +66,7 @@ tasks.getByName<PatchPluginXmlTask>("patchPluginXml") {
         ${readChangeNotes("CHANGELOG.md", version).orEmpty()}
         <p>
             To see full list of changes to this plugin refer to
-            <a href="${project.properties["repository"]}/blob/master/CHANGELOG.md">GitHub repo</a>.
+            <a href="${prop("repository")}/blob/master/CHANGELOG.md">GitHub repo</a>.
         </p>
         """.trimIndent()
     )
@@ -151,4 +113,62 @@ tasks {
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+
+val SourceSet.kotlin: SourceDirectorySet
+    get() =
+        (this as HasConvention)
+            .convention
+            .getPlugin(KotlinSourceSet::class.java)
+            .kotlin
+
+fun prop(name: String): String =
+    extra.properties[name] as? String
+        ?: error("Property `$name` is not defined in gradle.properties")
+
+fun readChangeNotes(pathname: String, version: String): String? {
+    val changeLog = file(pathname)
+    if (!changeLog.exists()) {
+        return null
+    }
+
+    val lines = changeLog.readLines()
+    val notes: MutableList<MutableList<String>> = mutableListOf()
+    var note: MutableList<String>? = null
+    var found = false
+
+    for (line in lines) {
+        if (!line.startsWith("## [$version") && !found) {
+            continue
+        }
+
+        if (line.startsWith("## [") && found) {
+            break
+        }
+
+        if (line.startsWith("## [$version")) {
+            note = mutableListOf()
+            notes.add(note)
+            found = true
+
+            continue
+        }
+
+        if (line.startsWith("- ") && found) {
+            note?.add(
+                line.trimStart('-', ' ').replace("`(.*?)`".toRegex()) {
+                    "<code>${it.groupValues[1]}</code>"
+                }
+            )
+        }
+    }
+
+    if (notes.size == 0) {
+        return null
+    }
+
+    return notes.joinToString("</li><li>", prefix = "<ul><li>", postfix = "</li></ul>") {
+        it.joinToString("</li><li>")
+    }
 }
